a32aed60b89f86a103872153bcb31956e3c7acd7
jcamhi <jeremshy@h3lp.fr>
2018-03-09 18:48:53 +0100

    On va dire que je commence a comprendrE

--- src/com/avaj_launcher/weather/WeatherProvider.java
+++ src/com/avaj_launcher/weather/WeatherProvider.java
@@ -1,12 +1,14 @@
 package com.avaj_launcher.weather;
 
+import java.util.ArrayList;
+
 public class WeatherProvider {
     private WeatherProvider()
     {
     }
 
     private static WeatherProvider weatherProvider = new WeatherProvider();
-    private static String weather;
+    private static String weather[] = {"RAIN", "FOG", "SUN", "SNOW"};
 
     public static WeatherProvider getProvider()
     {
